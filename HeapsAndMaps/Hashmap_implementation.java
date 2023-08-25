fd9d09d208699190411c4adb3e3ddd31ce81bcf3
Dheeruyendra Pratap Singh <56476885+Dheeruyendra@users.noreply.github.com>
2023-08-25 20:33:44 +0530

    updated implementation of hashmap

--- HeapsAndMaps/Hashmap_implementation.java
+++ HeapsAndMaps/Hashmap_implementation.java
@@ -1,134 +1,116 @@
 package HeapsAndMaps;
+ import java.lang.Object;
 import java.util.*;
 import java.io.*;
 
 public class Hashmap_implementation {
-class HashMap<K, V>{
-	
-		private class HMNode{
-			K key;
-            V value;
-			HMNode(K key, V value){
-				this.key = key;
-				this.value = value;
-			}
-		}
-		
-		private int size; //n
-		private LinkedList<HMNode>[] buckets;//array of linekd lists
-		
-		public HashMap(){
-			initbucket(4);
-			size = 0;
-		}
-		
-		//initialize bucket
-		private void initbucket(int N){
-			buckets = new LinkedList[N];
-			for(int bi =0; bi < N; bi++){
-				buckets[bi] = new LinkedList<>();
-			}
-		}
-		
-		//add key-value in map
-		public void put(K key, V value) throws Exception{
-			 int bi = hashfxn(key);
-			 int di = getIndexWithinBucket(key, bi);
-			 //if present => update value
-			 if(di != -1){
-				HMNode node = buckets[bi].get(di);
-				 node.value = value;
-			 }
-			 //not present => add key-value pair
-			 else{
-				HMNode node = new HMNode(key, value);
-				buckets[bi].add(node);
-				size+=1; 
-			 }
-			 double lambda = (size*1.0) / buckets.length;
-			 if(lambda > 2.0){
-				 rehash();
-			 }
-		}
-		
-		//helper methods
-		private void rehash() throws Exception{
-			LinkedList<HMNode>[] oba = buckets;
-			
-			initbucket(oba.length * 2);
-			size = 0;
-			
-			for(int i=0; i<oba.length; i++){
-				for(HMNode node : oba[i]){
-					put(node.key, node.value);
-				}
-			}	
-		}
-		private int hashfxn(K key){
-			int hc = key.hashCode();
-			return Math.abs(hc) % buckets.length;
-		}
-		private getIndexWithinBucket(K Key, int bi){
-			int di = 0;
-			for(HMNode node : buckets[bi]){
-				if(node.key.equals(key))return di;
-				di+=1;
-			}
-			return -1;
-		}
-		
-		//containsKey
-		public boolean containsKey(K key){
-			int bi = hashfxn(key);
-			int di = getIndexWithinBucket(key, bi);
-			
-			if(di == -1)return false;
-			else return true;
-		}
-		
-		//get value
-		public V get(K key){
-			int bi = hashfxn(key);
-			int di = getIndexWithinBucket(key, bi);
-			
-			if(di != -1){
-			HMNode node = buckets[bi].get(di);
-			return node.value;
-			}
-			else return null;
-		}
-		
-		//remove value
-		public V remove(K key) throws Exception{
-			int bi = hashfxn(key);
-			int di = getIndexWithinBucket(key, bi);
-			
-			if(di != -1){
-				HMNode node = buckets[bi].remove(di);
-				return node.value;
-				size-=1;
-			}
-			else{
-				return null;
-			}
-		}
-		
-		//size
-		public int size(){
-			return size;
-		}
-		
-		//get keySet
-		public ArrayList<K> keySet() throws Exception{
-			ArrayList<K> keys  = new ArrayList<>();
-			for(int i=0; i<buckets.length; i++){
-				for(HMNode node : buckets[i]){
-                  keys.add(node.key);
+   
+    class MyHashMap {
+        class HMNode{
+            int key, val;
+            HMNode(int key, int val){
+                this.key = key;
+                this.val = val;
+            }
+        }
+        LinkedList<HMNode>[] buckets;
+        int size;
+        public MyHashMap() {
+               initbuckets(4);
+               size = 0;
+        }
+    
+        private void initbuckets(int n){
+            buckets = new LinkedList[n];
+            for(int i=0; i<n; i++)buckets[i] = new LinkedList<>();
+        }
+    
+     
+        public void put(int key, int value) {
+            int bi = hashfxn(key); //bucket index
+            int di = getIndexWithinBucket(key, bi); //index within bucket
+    
+            if(di != -1){
+               HMNode node = buckets[bi].get(di);
+               node.val = value;
+            }
+            else{
+              HMNode node = new HMNode(key, value);
+              buckets[bi].add(node);
+              size+=1;
+            }
+    
+            double lambda = (size * 1.0)/buckets.length;
+            if(lambda > 2.0)rehash();
+        }
+        
+        public int get(int key) {
+                int bi = hashfxn(key);
+                int di = getIndexWithinBucket(key, bi);
+                if(di != -1){
+                    HMNode node = buckets[bi].get(di);
+                    return node.val;  
+                }   
+                return -1;
+        }
+        
+        public void remove(int key){
+               int bi = hashfxn(key);
+               int di = getIndexWithinBucket(key, bi);
+               if(di != -1){
+                  buckets[bi].remove(di);
+                  size-=1;
+               }
+        }
+        
+         public boolean containsKey(int key){
+                int bi = hashfxn(key);
+                int di = getIndexWithinBucket(key, bi);
+    
+                if(di != -1)return true;
+                else return false;
+         }
+         
+         public ArrayList<Integer> keySet(){
+                ArrayList<Integer> list = new ArrayList<>();
+    
+                for(int i=0; i<buckets.length; i++){
+                    for(HMNode node : buckets[i]){
+                        list.add(node.val);
+                    }
                 }  
-			}
-			
-			return keys;
-		}
-		
-	} 
+                return list;
+         }
+    
+    
+    //helper methods
+        private void rehash(){
+            LinkedList<HMNode>[] oba = buckets;
+            initbuckets(oba.length * 2);
+            size = 0;
+    
+            for(int i=0; i<oba.length; i++){
+                for(HMNode node : oba[i]){
+                    put(node.key, node.val);
+                }
+            }
+        }
+    
+        private int getIndexWithinBucket(int key, int bi){
+               int index = 0;
+               for(HMNode node : buckets[bi]){
+                   if(node.key == key)return index;
+                   index+=1;
+               }
+               return -1;               
+        }
+    
+        private int hashfxn(int key){
+            Integer temp = new Integer(key);
+            int hc = temp.hashCode();
+    
+             return Math.abs(hc) % buckets.length;
+        }
+       
+    }
 }
