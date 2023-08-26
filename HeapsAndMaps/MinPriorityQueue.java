0d9031e524832ff367691f0945bd77f48523a491
Dheeruyendra Pratap Singh <56476885+Dheeruyendra@users.noreply.github.com>
2023-08-26 06:14:49 +0530

    updated pq code

--- HeapsAndMaps/MinPriorityQueue.java
+++ HeapsAndMaps/MinPriorityQueue.java
@@ -11,6 +11,15 @@ class MinPriorityQueue {
 	public PriorityQueue(){
 		data = new ArrayList<>();
 	}
+		//O(N) constructor to make heap
+	public PriorityQueue(int[] arr){
+		data = new ArrayList<>();
+		for(int val : arr){
+			data.add(val);
+		}	
+			
+		for(int i = (data.size()/2)-1; i>=0; i--)heapifyDown(i);
+		}
 	 
 	 //add
 	 public void add(int val){
