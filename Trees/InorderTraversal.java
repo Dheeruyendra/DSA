b1d8cf4f3d0c2e959f2dd0df6c90045c065b65b8
Dheeruyendra <pratapsingh31dec2002@gmail.com>
2023-10-12 09:36:56 +0530

    inorder traversal

---
+++ Trees/InorderTraversal.java
@@ -0,0 +1,20 @@
+package Trees;
+
+public class InorderTraversal {
+     ArrayList<Integer> inorderTraversal(TreeNode root){
+        ArrayList<Integer> res = new ArrayList<>();
+        if(root == null)return res;
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = root;
+        while(curr != null || !st.isEmpty()){
+            while(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            curr = st.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+     }
+}
